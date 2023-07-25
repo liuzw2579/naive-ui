d776ab315380e7ef3af2849e9f0486bded8f10e9
刘战伟 <liuzw2579@qq.com>
2023-07-25 17:20:48 +0800

    refactor(scrollbar): remove wrapper component. fix(scrollbar): render scopedId in scrollbar. use `use-resize` instead `VResizeObserver`

--- src/_internal/scrollbar/index.ts
+++ src/_internal/scrollbar/index.ts
@@ -2,4 +2,8 @@ export {
   default as NScrollbar,
   XScrollbar as NxScrollbar
 } from './src/Scrollbar'
-export type { ScrollbarInst, ScrollbarProps } from './src/Scrollbar'
+export type {
+  ScrollbarInst,
+  ScrollbarTheme,
+  ScrollbarProps
+} from './src/Scrollbar'
